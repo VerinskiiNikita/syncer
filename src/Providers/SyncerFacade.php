ad6c1dd8a7b77074f7226668137730bd47723e72
nikitaverinskij <nikita@solvy.nl>
2023-04-21 14:08:03 +0300

    add anotationas

--- src/Providers/SyncerFacade.php
+++ src/Providers/SyncerFacade.php
@@ -4,6 +4,15 @@ namespace Sotoro\Syncer\Providers;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static bool isMaster()
+ * @method static bool setBalances(array $array)
+ * @method static bool setBalances(array $array)
+ * @method static integer getBalance(string $key)
+ * @method static integer getReserved(string $key)
+ * @method static integer getAvailable(string $key)
+ * @method static integer reserve(string $key, string $id, int $quantity = 1)
+ */
 class Syncer extends Facade
 {
 
