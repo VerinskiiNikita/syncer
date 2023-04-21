ec95d9f47c76b2af65c9672c91c2af525fa2c8b1
nikitaverinskij <nikita@solvy.nl>
2023-04-21 15:35:07 +0300

    syncer

--- src/Providers/SyncerServiceProvider.php
+++ src/Providers/SyncerServiceProvider.php
@@ -2,10 +2,15 @@
 
 namespace Sotoro\Syncer\Providers;
 
+use Illuminate\Cache\CacheManager;
+use Illuminate\Cache\RedisStore;
 use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Redis\RedisManager;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 use Sotoro\Syncer\Server;
+use Sotoro\Syncer\Syncer;
 
 class SyncerServiceProvider extends ServiceProvider implements DeferrableProvider
 {
@@ -28,6 +33,27 @@ class SyncerServiceProvider extends ServiceProvider implements DeferrableProvide
         $this->app->singleton('syncer', function ($app) {
             return new Syncer($app['syncer.master'], $app['syncer.current'], $app['syncer.slaves']);
         });
+
+        $this->app->singleton('syncer.redis', function ($app) {
+            $config = $app->make('config')->get('database.syncer', []);
+            return new RedisManager($app, Arr::pull($config, 'client', 'phpredis'), $config);
+        });
+
+        $this->app->bind('syncer.connection', function ($app) {
+            return $app['syncer.redis']->connection();
+        });
+
+        $this->app->bind('syncer.cache', function ($app) {
+            $connection = $app['syncer.connection'];
+            $store = new RedisStore($app['redis'], '', $connection);
+            return (new CacheManager($app))->repository(
+                $store->setLockConnection($connection)
+            );
+        });
+
+        $this->app->when(Syncer::class)->needs(Cache::class)->give(function () {
+            return $this->app['syncer.cache'];
+        });
     }
 
     public function boot()
@@ -39,6 +65,6 @@ class SyncerServiceProvider extends ServiceProvider implements DeferrableProvide
 
     public function provides()
     {
-        return ['syncer', 'syncer.master', 'syncer.slaves'];
+        return ['syncer', 'syncer.master', 'syncer.slaves', 'syncer.cache', 'syncer.connection', 'syncer.redis'];
     }
 }
